e930276e291c84b6806d1bb4e6c2f06f49ac5578
Piyush <apiyushgarg2006@gmail.com>
2026-07-11 22:57:45 +0530

    Added questions

---
+++ CP31/1000/Fair_numbers.cpp
@@ -0,0 +1,64 @@
+https://codeforces.com/problemset/problem/1411/B
+/*Fair Numbers
+
+We call a positive integer number fair if it is divisible by each of its nonzero digits. For example, 102 is fair (because it is divisible by 1 and 2), but 282 is not, because it isn't divisible by 8.
+Given a positive integer n. Find the minimum integer x, such that n≤x and x is fair.
+
+Input
+The first line contains the number of test cases t (1≤t≤10^3).
+Each of the next t lines contains an integer n (1≤n≤10^18).
+
+Output
+For each of the t test cases, print a single integer — the least fair number, which is not less than n.
+Example
+
+Input
+4
+1
+282
+1234567890
+1000000000000000000
+
+Output
+1
+288
+1234568040
+1000000000000000000
+
+Note
+Explanations for some test cases:
+• In the first test case, the number 1 is fair itself.
+• In the second test case, the number 288 is fair (it's divisible by both 2 and 8). None of the numbers from [282,287] is fair, because, for example, none of them is divisible by 8.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+bool is_fair(long long n)
+{
+    long long m = n;
+    while (n)
+    {
+        int d = n % 10;
+        if (d != 0 && m % d != 0)
+            return false;
+        n /= 10;
+    }
+    return true;
+}
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        while (true)
+        {
+            if (is_fair(n))
+                break;
+            n++;
+        }
+        cout << n << "\n";
+    }
+}
+// TC - O(nlogn)  SC - O(1)
