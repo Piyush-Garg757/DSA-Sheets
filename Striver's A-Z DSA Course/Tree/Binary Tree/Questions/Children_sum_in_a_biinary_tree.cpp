9812fb849363d2de6c6ad9966cbdacf995f1d2a3
Piyush <apiyushgarg2006@gmail.com>
2026-08-06 12:25:42 +0530

    Added questions

--- Striver's A-Z DSA Course/Tree/Binary Tree/Questions/Children_sum_in_a_biinary_tree.cpp
+++ Striver's A-Z DSA Course/Tree/Binary Tree/Questions/Children_sum_in_a_biinary_tree.cpp
@@ -46,3 +46,118 @@ public:
     }
 };
 // TC - O(n)  SC - O(n)
+
+// What if
+/*Given a binary tree, modify the tree so that it satisfies the Children Sum Property.
+
+Children Sum Property:
+For every non-leaf node,
+node->data = left->data + right->data
+
+Rules:
+You are allowed only to increment node values.
+You cannot decrement any node's value.
+Structure of the tree should remain unchanged.*/
+
+class Solution
+{
+public:
+    void changeTree(Node *root)
+    {
+        if (root == NULL)
+            return;
+        int child = 0;
+        if (root->left)
+            child += root->left->data;
+        if (root->right)
+            child += root->right->data;
+        if (child >= root->data)
+            root->data = child;
+        else
+        {
+            if (root->left)
+                root->left->data = root->data;
+            else if (root->right)
+                root->right->data = root->data;
+        }
+        changeTree(root->left);
+        changeTree(root->right);
+        int total = 0;
+        if (root->left)
+            total += root->left->data;
+        if (root->right)
+            total += root->right->data;
+        if (root->left || root->right)
+            root->data = total;
+    }
+};
+// TC - O(n)  SC - O(n)
+/*Sabse bada intuition
+Question me likha hai
+Decrease nahi kar sakte. Sirf increase kar sakte ho.
+Isi ek line ki wajah se pura algorithm banta hai.
+Case 1
+Suppose
+      10
+     /  \
+    4    8
+Children sum
+4+8=12
+Parent
+10
+Already
+12>=10
+To simply
+parent = children sum
+Kar do.
+Result
+      12
+     /  \
+    4    8
+Koi problem nahi.
+Case 2
+Ab dekho
+      20
+     /  \
+    5    6
+Children sum
+11
+Parent
+20
+Property fail.
+Ab kya kare?
+Parent ko
+11
+Nahi bana sakte.
+Kyuki decrease allowed hi nahi.
+To option kya bacha?
+Children ko bada karo.
+Suppose
+left=20
+Tree
+      20
+     /  \
+   20    6
+Ab recursion baad me ise aur fix kar dega.
+Isi wajah se code me likha hai
+if(root->left)
+    root->left->data = root->data;
+Top Down
+
+Pehla phase
+Parent se children ki taraf.
+Purpose?
+Kabhi bhi child parent se chhota na rahe.
+Code
+if(child>=root->data)
+    root->data=child;
+else
+    child=root->data;
+Ye sirf tree ko prepare karta hai.
+Bottom Up
+Recursion ke baad
+changeTree(root->left);
+changeTree(root->right);
+Children already valid ban chuke hain.
+Ab
+root->data=left+right;*/
