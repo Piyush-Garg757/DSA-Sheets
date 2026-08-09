f2136f5cba26951b0bf353114c8a8f1cf51565c5
Piyush <apiyushgarg2006@gmail.com>
2026-08-09 14:15:20 +0530

    Added questions

---
+++ Striver's A-Z DSA Course/Tree/Binary Search Tree/Questions/Validate_BST.cpp
@@ -0,0 +1,46 @@
+/*Validate Binary Search Tree
+
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+A valid BST is defined as follows:
+The left subtree of a node contains only nodes with keys strictly less than the node's key.
+The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input: root = [2,1,3]
+Output: true
+
+Example 2:
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool solve(TreeNode *root, long long l, long long r)
+    {
+        if (!root)
+            return true;
+        if (root->val > l && root->val < r)
+            return solve(root->left, l, root->val) && solve(root->right, root->val, r);
+        return false;
+    }
+    bool isValidBST(TreeNode *root)
+    {
+        return solve(root, LLONG_MIN, LLONG_MAX);
+    }
+};
+// TC - O(n)  SC - O(n)
