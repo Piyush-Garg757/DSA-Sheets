a9198455055933a6ecab30ade8bab83a171d851f
Piyush <apiyushgarg2006@gmail.com>
2026-08-27 21:42:19 +0530

    Added questions

---
+++ Striver's A-Z DSA Course/Graph/Questions/Cycle_detection_in_directed_graph_using_bfs.cpp
@@ -0,0 +1,34 @@
+// Logic simple hai   we know topo sort works only for DAG   so if there is no cycle  it should work fine and ans.size() == v  but if there is a cycle then queeu becomes empty earlier and the condition fails     which we can use to detect cycle
+class Solution
+{
+public:
+    bool isCyclic(int v, vector<vector<int>> &a)
+    {
+        // code here
+        vector<int> indegree(v, 0);
+        vector<int> ans;
+        vector<int> adj[v];
+        queue<int> q;
+        for (int i = 0; i < a.size(); i++)
+        {
+            indegree[a[i][1]]++;
+            adj[a[i][0]].push_back(a[i][1]);
+        }
+        for (int i = 0; i < v; i++)
+            if (indegree[i] == 0)
+                q.push(i);
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for (auto i : adj[node])
+            {
+                indegree[i]--;
+                if (indegree[i] == 0)
+                    q.push(i);
+            }
+        }
+        return ans.size() != v;
+    }
+};
